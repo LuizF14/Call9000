5b7e68357a85fba24880102397575b6215307808
LuizF14 <54507701+LuizF14@users.noreply.github.com>
2021-02-04 01:27:12 -0300

    Puppeteer and heroku fix 2

--- whatsapp-api/whatsapp-bot.js
+++ whatsapp-api/whatsapp-bot.js
@@ -3,14 +3,30 @@ const venom = require('venom-bot');
 
 module.exports = async (command) => {
     try {
-        const client = await venom.create({
-            puppeteerOptions: {
-                args: [
+        const client = await venom.create(//session
+            'sessionName', //Pass the name of the client you want to start the bot
+            //catchQR
+            (base64Qrimg, asciiQR, attempts, urlCode) => {
+              console.log('Number of attempts to read the qrcode: ', attempts);
+              console.log('Terminal qrcode: ', asciiQR);
+              console.log('base64 image string qrcode: ', base64Qrimg);
+              console.log('urlCode (data-ref): ', urlCode);
+            },
+            // statusFind
+            (statusSession, session) => {
+              console.log('Status Session: ', statusSession); //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken
+              //Create session wss return "serverClose" case server for close
+              console.log('Session name: ', session);
+            },
+            // options
+            {
+              puppeteerOptions: {
+                    'args' : [
                     '--no-sandbox',
-                    '--disable-setuid-sandbox',
-                  ],
-            }
-        });
+                    '--disable-setuid-sandbox'
+                  ]
+              }
+            },);
         client.onMessage(async message => {
             if(message.body === command) {
                 const response = await getNextClass();
